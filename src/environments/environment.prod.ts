c81fcccf8023940859fe4d438b8c7533cea6b301
Ruta89 <arek.ruta89@gmail.com>
2019-06-10 15:24:18 +0200

    waer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDQECzeZrYQrmBnGvEpEo1LRKazzDYMZp4",
-    authDomain: "pwaarek.firebaseapp.com",
-    databaseURL: "https://pwaarek.firebaseio.com",
-    projectId: "pwaarek",
-    storageBucket: "pwaarek.appspot.com",
-    messagingSenderId: "139758750414",
-    appId: "1:139758750414:web:c8a0412e7a6adeba"
+    apiKey: 'AIzaSyDQECzeZrYQrmBnGvEpEo1LRKazzDYMZp4',
+    authDomain: 'pwaarek.firebaseapp.com',
+    databaseURL: 'https://pwaarek.firebaseio.com',
+    projectId: 'pwaarek',
+    storageBucket: 'pwaarek.appspot.com',
+    messagingSenderId: '139758750414',
+    appId: '1:139758750414:web:c8a0412e7a6adeba'
   }
 };
